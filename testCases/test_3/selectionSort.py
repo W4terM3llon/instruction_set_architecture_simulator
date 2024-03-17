4c78a6281131e396f4e5d5d1683e3909731b80c7
W4terM3llon <75610693+W4terM3llon@users.noreply.github.com>
2024-03-17 19:01:23 +0100

    remove comment

--- testCases/test_3/selectionSort.py
+++ testCases/test_3/selectionSort.py
@@ -1,15 +1,10 @@
 def selectionSort(array: list):
     for i in range(len(array)):
-
-        # Find the minimum element in remaining
-        # unsorted array
         min_idx = i
         for j in range(i + 1, len(array)):
             if array[min_idx] > array[j]:
                 min_idx = j
 
-                # Swap the found minimum element with
-        # the first element
         array[i], array[min_idx] = array[min_idx], array[i]
 
 
